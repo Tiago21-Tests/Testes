c7b20230899bf13697eba59d866df0f2632119bc
Tiago Costa <srpmecatronica@gmail.com>
2022-04-11 12:30:34 -0300

    Implemented a new functionality

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <map>
 
 int main(){
 
+    std::map<char, bool> myMap;
+    char letter;
+    myMap['T'] = true;
+    myMap['i'] = true;
+
     std::cout << "Git Practice!" << std::endl;
+    std::cout << ">> ";
+    std::cin >> letter;
+
+    if(myMap.find(letter) == myMap.end()){
+        std::cout << "There is no letter!" << std::endl;
+    }
+    else{
+        std::cout << "There is the letter!" << std::endl;
+    }
+
     return 0;
 }
