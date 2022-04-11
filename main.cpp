8ace7fa68c5447230d0896d72a0baaef72609b61
Tiago Costa <srpmecatronica@gmail.com>
2022-04-11 16:11:51 -0300

    Vector functionality has been implemented!

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <map>
+#include "my_vector.hpp"
 
 int main(){
 
     std::map<char, bool> myMap;
+    std::vector<char> vec;
     char letter;
+    int opt;
     myMap['T'] = true;
     myMap['i'] = true;
 
@@ -18,6 +21,18 @@ int main(){
     else{
         std::cout << "There is the letter!" << std::endl;
     }
+
+    do{
+        std::cout << "Character >> ";
+        std::cin >> letter;
+        appendAnItem(vec, letter);
+        std::cout << std::endl;
+        std::cout << "Opt: ";
+        std::cin >> opt;
+
+    }while(opt != 0);
+
+    printVector(vec);
 
     return 0;
 }
